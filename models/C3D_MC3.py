559f6e04e84802da9424a12f933a17565eaae055
random_user <farabi.shafkat@gmail.com>
2020-07-03 23:12:21 +0600

    pretrain script better;

--- models/C3D_MC3.py
+++ models/C3D_MC3.py
@@ -19,12 +19,12 @@ randomseed = 0
 torch.manual_seed(randomseed); torch.cuda.manual_seed_all(randomseed); random.seed(randomseed); np.random.seed(randomseed)
 
 
-class C3D_MC5(nn.Module):
+class C3D_MC3(nn.Module):
     """
     The C3D network as described in [1].
     """
     def __init__(self):
-        super(C3D_MC5, self).__init__()
+        super(C3D_MC3, self).__init__()
 
         self.conv1 = nn.Conv3d(3, 64, kernel_size=(3, 3, 3), padding=(1, 1, 1))
         self.pool1 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
@@ -32,13 +32,13 @@ class C3D_MC5(nn.Module):
         self.conv2 = nn.Conv3d(64, 128, kernel_size=(3, 3, 3), padding=(1, 1, 1))
         self.pool2 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
 
-        self.conv3a = nn.Conv3d(128, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.conv3b = nn.Conv3d(256, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.pool3 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+        self.conv3a = nn.Conv2d(1024, 2048, kernel_size=(3, 3), padding=(1, 1))
+        self.conv3b = nn.Conv2d(2048, 1024, kernel_size=(3, 3), padding=(1, 1))
+        self.pool3 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
 
-        self.conv4a = nn.Conv3d(256, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.conv4b = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.pool4 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+        self.conv4a = nn.Conv2d(1024, 2048, kernel_size=(3, 3), padding=(1, 1))
+        self.conv4b = nn.Conv2d(2048, 1024, kernel_size=(3, 3), padding=(1, 1))
+        self.pool4 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
 
         self.conv5a = nn.Conv2d(1024, 1024, kernel_size=(3, 3), padding=(1, 1))
         self.conv5b = nn.Conv2d(1024, 512, kernel_size=(3, 3), padding=(1, 1))
@@ -55,16 +55,20 @@ class C3D_MC5(nn.Module):
         h = self.relu(self.conv2(h))
         h = self.pool2(h)
 
+        h= h.view(h.shape[0],-1,h.shape[-2],h.shape[-1])
+
         h = self.relu(self.conv3a(h))
         h = self.relu(self.conv3b(h))
         h = self.pool3(h)
+        
+        
 
         h = self.relu(self.conv4a(h))
         h = self.relu(self.conv4b(h))
         h = self.pool4(h)
 
        # print(h.shape)
-        h= h.view(h.shape[0],-1,h.shape[-2],h.shape[-1])
+      
        # print(h.shape)
 
 
--- models/c3d_seperable.py
+++ models/c3d_seperable.py
@@ -19,31 +19,62 @@ randomseed = 0
 torch.manual_seed(randomseed); torch.cuda.manual_seed_all(randomseed); random.seed(randomseed); np.random.seed(randomseed)
 
 
-class C3D_MC5(nn.Module):
+class STConv3d(nn.Module):
+    def __init__(self,in_planes,out_planes,kernel_size,stride,padding=0):
+        super(STConv3d, self).__init__()
+        self.conv = nn.Conv3d(in_planes, out_planes, kernel_size=(1,kernel_size,kernel_size),stride=(1,stride,stride),padding=(0,padding,padding))
+        self.conv2 = nn.Conv3d(out_planes,out_planes,kernel_size=(kernel_size,1,1),stride=(stride,1,1),padding=(padding,0,0))
+
+       # self.bn=nn.BatchNorm3d(out_planes, eps=1e-3, momentum=0.001, affine=True)
+        self.relu = nn.ReLU(inplace=True)
+        
+        #self.bn2=nn.BatchNorm3d(out_planes, eps=1e-3, momentum=0.001, affine=True)
+        self.relu2=nn.ReLU(inplace=True)
+        
+        #nn.init.normal(self.conv2.weight,mean=0,std=0.01)
+        #nn.init.constant(self.conv2.bias,0)
+    
+    def forward(self,x):
+        x=self.conv(x)
+        #x=self.conv2(x)
+      #  x=self.bn(x)
+        x=self.relu(x)
+        x=self.conv2(x)
+        #x=self.bn2(x)
+        x=self.relu2(x)
+        return x
+
+
+
+
+
+
+class C3D_SP(nn.Module):
     """
     The C3D network as described in [1].
     """
-    def __init__(self):
-        super(C3D_MC5, self).__init__()
+        
 
-        self.conv1 = nn.Conv3d(3, 64, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+    def __init__(self):
+        super(C3D_SP, self).__init__()
+
+        self.conv1 = STConv3d(3, 64, kernel_size=3,stride=1, padding=1)
         self.pool1 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
 
-        self.conv2 = nn.Conv3d(64, 128, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.conv2 = STConv3d(64, 128, kernel_size=3,stride=1, padding=1)
         self.pool2 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
 
-        self.conv3a = nn.Conv3d(128, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.conv3b = nn.Conv3d(256, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.conv3a = STConv3d(128, 256, kernel_size=3, padding=1,stride=1)
+        self.conv3b = STConv3d(256, 256, kernel_size=3, padding=1,stride=1)
         self.pool3 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
 
-        self.conv4a = nn.Conv3d(256, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.conv4b = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.conv4a = STConv3d(256, 512, kernel_size=3, padding=1,stride=1)
+        self.conv4b = STConv3d(512, 512, kernel_size=3, padding=1,stride=1)
         self.pool4 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
 
-        self.conv5a = nn.Conv2d(1024, 1024, kernel_size=(3, 3), padding=(1, 1))
-        self.conv5b = nn.Conv2d(1024, 512, kernel_size=(3, 3), padding=(1, 1))
-        
-        self.pool5 = nn.MaxPool2d(kernel_size=( 2, 2), stride=(2, 2), padding=(1, 1))
+        self.conv5a = STConv3d(512, 512, kernel_size=3, padding=1,stride=1)
+        self.conv5b = STConv3d(512, 512, kernel_size=3, padding=1,stride=1)
+        self.pool5 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2), padding=(0, 1, 1))
 
         self.relu = nn.ReLU()
 
@@ -63,18 +94,12 @@ class C3D_MC5(nn.Module):
         h = self.relu(self.conv4b(h))
         h = self.pool4(h)
 
-       # print(h.shape)
-        h= h.view(h.shape[0],-1,h.shape[-2],h.shape[-1])
-       # print(h.shape)
-
-
         h = self.relu(self.conv5a(h))
         h = self.relu(self.conv5b(h))
         h = self.pool5(h)
-     
+
         h = h.view(-1, 8192)
         return h
-
 
 """
 References
@@ -84,7 +109,7 @@ Proceedings of the IEEE international conference on computer vision. 2015.
 """
 
 if __name__ == "__main__":
-    c3d = C3D_MC5()
+    c3d = C3D_SP()
     dummy_clip =  torch.zeros(1,3, 16, 112, 112)
     print(dummy_clip.shape)
     h = c3d(dummy_clip)
